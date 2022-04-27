c1e7f8df1354b591d424b368ed75523dfae92321
MeganTherion <meganhutt@hotmail.com>
2022-04-27 23:18:38 +0000

    "add express server changes"

--- express_server.js
+++ express_server.js
@@ -15,6 +15,21 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+//add response with html to be rendered in browser:
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n")
+});
+
+// will a variable created in one request be available in another?
+app.get("/set", (req, res) => {
+  const a = 1;
+  res.send(`a = ${a}`);
+});
+//now check to see if it's available:
+app.get("/fetch", (req, res) => {
+  res.send(`a = ${a}`)
+}); //not available!
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
